3bcf6a4a02c50f7c3897b2e328c7b2b6c40ccbc3
Pavel Kulebyakin <pavel.kulebyakin.study@gmail.com>
2024-10-05 23:37:51 +0300

    Add serializable implementation to itemdto class

--- src/main/java/org/spring/project/dto/ItemDto.java
+++ src/main/java/org/spring/project/dto/ItemDto.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ItemDto {                              // TODO check constructors
-    private Long id;
+public class ItemDto implements Serializable {                              // TODO check constructors
+    private long id;
     private String name;
     private int quantity;
 }
