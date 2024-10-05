ed3a6ab57e6d18be1b242b2562fe00a307ab4b48
Pavel Kulebyakin <pavel.kulebyakin.study@gmail.com>
2024-10-05 23:36:07 +0300

    Change sql schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS item (
     id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    quantity INT NOT NULL
 );
 
-INSERT INTO item (name) VALUES ('Sample Item 1');
-INSERT INTO item (name) VALUES ('Sample Item 2');
+INSERT INTO item (name, quantity) VALUES ('Sample Item 1', 2);
+INSERT INTO item (name, quantity) VALUES ('Sample Item 2', 5);
